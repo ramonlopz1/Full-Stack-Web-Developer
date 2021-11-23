e2c63fdc14fb0baf25327d1dff458549b2054c54
ramonlopz1 <davidramon123@hotmail.com>
2021-11-23 07:24:07 -0300

    learning

--- exercicios-js/Esnext/promise_5.js
+++ exercicios-js/Esnext/promise_5.js
@@ -13,4 +13,13 @@ function funcionarOuNao(valor, chanceErro) {
 funcionarOuNao('Testando...', 1) // se passar 1 será reject, se passar 0.1 será resolve
     .then(v => console.log(`Valor: ${v}`))
     .catch(err => console.log(`Erro: ${err}`)) // tratando o erro
-    
+     
+
+/**
+ * functionarOuNao('Testenado...', 0.5)
+ *  .then(v => v)
+ *  . then( v => console.log, err => consolelog(erro)
+*             
+ * )
+ * 
+ */
