e00997923e55d05e611dfe4da1c076e363cbe4fe
ramonlopz1 <davidramon123@hotmail.com>
2021-12-18 18:07:57 -0300

    learning

--- projeto2/src/js/script.js
+++ projeto2/src/js/script.js
@@ -22,32 +22,59 @@ document.querySelectorAll('main section div a').forEach(valor => {
     const array = []
     let contador = 0
 
+
+    
+    console.log(valor)
     
     valor.onclick = (e) => {
         e.preventDefault()
+
         
         array.push(contador++)
-        if (valor.hasAttribute = 'c1' && array.length < 2) {
-            agilityCard.style.display = 'none'
-            qualityCard.style.display = 'none'
+        
+        if (array.length <= 1) {
+            textDiv.className = 'textDiv'
+
+            if (valor.hasAttribute('c1')) {
+                agilityCard.style.display = 'none'
+                qualityCard.style.display = 'none'
+
+                textDiv.innerHTML = 
+                `<h1>Segurança em primeiro lugar</h1>
+                 <h3>Proteção de ponta a ponta</h3> 
+                 <p>Seu website completamente criptografado, com os melhores certificados de segurança do World Wide Web.</p>`
+            } else if (valor.hasAttribute('c2')) {
+                securityCard.style.display = 'none'
+                qualityCard.style.display = 'none'
+
+                textDiv.innerHTML = 
+                `<h1>Agilidade essencial</h1>
+                 <h3>Scrum Team para entrega de qualidade máxima</h3> 
+                 <p>Equipe focada em qualidade, eficiência e eficácia.</p>`
+            } else if (valor.hasAttribute('c3')) {
+                agilityCard.style.display = 'none'
+                securityCard.style.display = 'none'
+
+                textDiv.innerHTML = 
+                `<h1>Qualidade para o seu negócio</h1>
+                <h3>Design, regras de negócio, relações de dados e muito mais para você.</p>`
+            }
 
             
-            textDiv.className = 'textDiv'   
-            textDiv.innerHTML = 
-            `<h1>Segurança em primeiro lugar</h1>
-             <h3>Proteção de ponta a ponta</h3> 
-             <p>Seu website completamente criptografado, com os melhores certificados de segurança do World Wide Web.</p>`
+               
+            
 
             mainSection.appendChild(textDiv)
 
-            console.log(array)
             valor.style.transform = 'rotate(180deg)'
+
         } else if (array.length = 1) {
             agilityCard.style.display = 'flex'
             qualityCard.style.display = 'flex'
+            securityCard.style.display = 'flex'
+
             mainSection.removeChild(textDiv)
             array.pop()
-
             valor.style.transform = 'rotate(360deg)'
         }
         
