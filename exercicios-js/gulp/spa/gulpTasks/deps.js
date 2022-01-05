308b0bbbf55344d93cee2b20f25037d1c990fcf8
ramonlopz1 <davidramon123@hotmail.com>
2022-01-05 11:03:15 -0300

    learning

--- exercicios-js/gulp/spa/gulpTasks/deps.js
+++ exercicios-js/gulp/spa/gulpTasks/deps.js
@@ -1,11 +1,17 @@
 const gulp = require('gulp')
+const uglifycss = require('gulp-uglifycss')
+const concat = require('gulp-concat')
 
-const depsCSS = cb => {
-    return cb()
+const depsCSS = () => {
+    return gulp.src('node_modules/font-awesome/css/font-awesome.css')
+        .pipe(uglifycss({ "uglyComments": false }))
+        .pipe(concat('dep.min.css'))
+        .pipe(gulp.dest('build/assets/css'))
 }
 
-const depsFonts = cb => {
-    return cb()
+const depsFonts = () => {
+    return gulp.src('node_modules/font-awesome/fonts/*.*')
+        .pipe(gulp.dest('build/assets/fonts'))
 }
 
 module.exports = {
--- exercicios-js/gulp/spa/gulpTasks/servidor.js
+++ exercicios-js/gulp/spa/gulpTasks/servidor.js
@@ -1,12 +1,25 @@
 const gulp = require('gulp')
-
-function monitorarArquivos(cb) {
-    return cb()
-}
+const webserver = require('gulp-webserver')
+const watch = require('gulp-watch')
 
 function servidor(cb) {
+    return gulp.src('build')
+        .pipe(webserver({
+            port: 8080,
+            open: true,
+            livereload: true,
+        }))
+}
+
+function monitorarArquivos(cb) {
+    watch('src/**/*.html', () => gulp.series('appHTML')()) // sempre que alterar o html chamará a função appHTML
+    watch('src/**/*.cs', () => gulp.series('appCSS')())
+    watch('src/**/*.js', () => gulp.series('appJS')())
+    watch('src/assets/imgs/**/*.*', () => gulp.series('appIMG')())
     return cb()
 }
+
+
 
 module.exports = {
     monitorarArquivos,
