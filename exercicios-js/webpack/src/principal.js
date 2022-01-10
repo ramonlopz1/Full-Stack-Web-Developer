b3bfa4e20526b60987becd46f670fa983b883d07
ramonlopz1 <davidramon123@hotmail.com>
2022-01-10 17:49:06 -0300

    webpack

--- exercicios-js/webpack/src/principal.js
+++ exercicios-js/webpack/src/principal.js
@@ -1,5 +1,5 @@
 import Pessoa from './pessoa'
-import './assets/css/estilo.css'
+import './assets' //buscar o javascript dentro da pasta
 
 const atendente = new Pessoa
 
--- exercicios-js/webpack/webpack.config.js
+++ exercicios-js/webpack/webpack.config.js
@@ -1,25 +1,39 @@
 const webpack = require('webpack')
 const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const modoDev = process.env.NODE_ENV !== 'production'
+const UglifyJsPlugin = require('uglifyjs-webpack-plugin')
+const OptimizeCSSAssetsPlugin = require('optimize-css-assets-webpack-plugin')
+
 
 module.exports = {
-    mode: 'development', //ou production: ja minifica o código, melhorandoo para deployment
+    mode: modoDev ? 'development' : 'production',
     entry: './src/principal.js',
     output: {
         filename: 'principal.js',
         path: __dirname + '/public'
     },
+    optimization: {
+        minimizer: [
+            new UglifyJsPlugin({
+                cache: true,
+                parallel: true
+            }),
+            new OptimizeCSSAssetsPlugin({})
+        ]
+    },
     plugins: [
         new MiniCssExtractPlugin({
-            filename: "estilo.css"
+            filename: "estilo.css" //nome do arquivo que será gerado a partir do import no arquivo principal.js
         })
     ],
-    module: {
+    module: { //configura o css
         rules: [{
-            test: /\.css$/,
+            test: /\.s?[ac]ss$/,  // rejex: vai pesquisar arquivos css, sass ou scss
             use: [
                 MiniCssExtractPlugin.loader,
                 // 'style-loader', // Adiciona CSS a DOM injetando a tag <style>
-                'css-loader' // Interpreta @import, url()...
+                'css-loader', // Interpreta @import, url()...
+                'sass-loader',
             ]
         }]
     }
