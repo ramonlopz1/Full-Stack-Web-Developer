8b66cb17a60bb6fe6d1699973f485484c1804a0d
ramonlopz1 <davidramon123@hotmail.com>
2022-01-13 17:52:19 -0300

    section-reserve

--- arawi/src/assets/js/jquery/jquery.js
+++ arawi/src/assets/js/jquery/jquery.js
@@ -7,26 +7,35 @@
     const span = document.createElement('span')
     const radio = document.querySelectorAll('#section-translate input[type=radio]')
 
+    // Immediately apply elements
+    h1.innerHTML = h1Array[contador]
+    span.innerHTML = spanArray[contador]
+    radio[contador].checked = true
     $('#section-translate').css('background-image', `url(assets/images/section-translate/${contador}.jpg)`)
-    $('#section-translate').prepend(h1)
-    $('#section-translate').prepend(span)
 
+    $('#section-translate').prepend(span)
+    $('#section-translate').prepend(h1)
+    //
+
+    // Change radios with click and index position    
+    radio.forEach((retorno, indice) => {
+        retorno.onclick = () => {
+            $('#section-translate').css('background-image', `url(assets/images/section-translate/${indice}.jpg)`)
+            h1.innerHTML = h1Array[indice]
+            span.innerHTML = spanArray[indice]
+        }
+    })
+    //
+    
+    // Looping
     setInterval(() => {
-        radio.forEach((retorno, indice) => {
-            retorno.onclick = () => {
-                contador = indice
-            }
-        })
+        contador++;
+        contador >= 3 ? contador = 0 : contador
 
         h1.innerHTML = h1Array[contador]
         span.innerHTML = spanArray[contador]
         radio[contador].checked = true
 
         $('#section-translate').css('background-image', `url(assets/images/section-translate/${contador}.jpg)`)
-    
-        contador++;
-        contador >= 3 ? contador = 0 : contador
     }, 4000)
 })()
-
-
