b7841d272e300dde6434a1fd729091d40be6ed5a
ramonlopz1 <davidramon123@hotmail.com>
2022-01-13 11:23:05 -0300

    learning

--- arawi/src/assets/js/jquery/jquery.js
+++ arawi/src/assets/js/jquery/jquery.js
@@ -5,18 +5,38 @@
     const spanArray = ["Span 1", "Span 2", "Span 3"]
     const h1 = document.createElement('h1')
     const span = document.createElement('span')
+    const radio = document.querySelectorAll('#section-translate input[type=radio]')
     
+
     $('#section-translate').css('background-image', `url(assets/images/section-translate/${contador}.jpg)`)
-    $('#section-translate').append(h1)
-    $('#section-translate').append(span)
+    $('#section-translate').prepend(h1)
+    $('#section-translate').prepend(span)
     
+
+    
+
+    
+
     setInterval(() => {
+        radio.forEach((retorno, indice) => {
+            retorno.onclick = () => {
+                contador = indice
+            }
+        })
+
         h1.innerHTML = h1Array[contador]
         span.innerHTML = spanArray[contador]
+        radio[contador].checked = true
 
         $('#section-translate').css('background-image', `url(assets/images/section-translate/${contador}.jpg)`)
     
         contador++;
         contador >= 3 ? contador = 0 : contador
     }, 4000)
+
+    
+    
+
 })()
+
+
